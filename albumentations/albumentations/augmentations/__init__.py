6dbf90adc1232b533796877b031399e22bc95245
sheepstar1 <83896001+sheepstar1@users.noreply.github.com>
2021-12-14 22:36:00 +0800

    Add files via upload

---
+++ albumentations/albumentations/augmentations/__init__.py
@@ -0,0 +1,15 @@
+# Common classes
+from .bbox_utils import *
+from .crops.functional import *
+from .crops.transforms import *
+
+# New transformations goes to individual files listed below
+from .domain_adaptation import *
+from .functional import *
+from .geometric.functional import *
+from .geometric.resize import *
+from .geometric.rotate import *
+from .geometric.transforms import *
+from .keypoints_utils import *
+from .transforms import *
+from .utils import *
